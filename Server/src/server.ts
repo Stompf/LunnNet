7dfcab26d8f991a132494d7e28641eed17e3e190
Stompf <anton.lundmark@gmail.com>
2018-02-08 22:20:31 +0100

    Only support http server

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,27 +1,15 @@
 import * as express from 'express';
 import * as socketIO from 'socket.io';
 import * as http from 'http';
-import * as https from 'https';
 import * as winston from 'winston';
-import * as fs from 'fs';
 import { Dictionary } from 'typescript-collections';
 import { MatchMaking } from './matchmaking';
 
 const port = process.env.PORT || 4444;
 const app = express();
 
-let server: http.Server | https.Server;
-
-if (process.env.NODE_ENV === 'production') {
-    winston.info('Starting HTTPS server...');
-    let privateKey = fs.readFileSync('/etc/letsencrypt/live/home.lunne.nu/privkey.pem', 'utf8');
-    let certificate = fs.readFileSync('/etc/letsencrypt/live/home.lunne.nu/fullchain.pem', 'utf8');
-    let credentials = { key: privateKey, cert: certificate };
-    server = https.createServer(credentials, app);
-} else {
-    winston.info('Starting HTTP server...');
-    server = http.createServer(app);
-}
+winston.info('Starting HTTP server...');
+const server = http.createServer(app);
 
 const currentConnections = new Dictionary<string, SocketIO.Socket>();
 const matchMaking = new MatchMaking();
