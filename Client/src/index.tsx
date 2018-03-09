8ffdd601b88f0a309137a106c849b6d2d9f8d5db
Stompf <anton.lundmark@gmail.com>
2018-03-09 12:08:47 +0100

    redesigned menu

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -7,32 +7,37 @@ import Asteroids from './asteroids/asteroids';
 import AirHockey from './air-hockey/air-hockey-main-menu';
 import AirHockeyLocal from './air-hockey/air-hockey-local';
 import AirHockeyNetwork from './air-hockey/air-hockey-network';
-import { Reboot, AppBar, Button } from 'material-ui';
+import { Reboot, AppBar, Button, Grid } from 'material-ui';
 
 (window as any).PIXI = require('phaser-ce/build/custom/pixi');
 (window as any).p2 = require('phaser-ce/build/custom/p2');
 (window as any).Phaser = require('phaser-ce/build/custom/phaser-split');
 
-const AirHockeyLink = (props: any) => <Link to="/" {...props} />;
 const AsteroidLink = (props: any) => <Link to="/asteroids" {...props} />;
 
 const Header = () => (
     <AppBar position="static">
-        <div>
-            <Button component={AirHockeyLink}>Air hockey</Button>
-            <Button component={AsteroidLink}>Asteroids</Button>
-        </div>
+        <Grid container spacing={0}>
+            <Grid item>
+                <AirHockey />
+            </Grid>
+            <Grid item>
+                <Button color="inherit" component={AsteroidLink}>
+                    Asteroids
+                </Button>
+            </Grid>
+        </Grid>
     </AppBar>
 );
 
 const Main = () => (
     <main>
         <Switch>
-            <Route exact={true} path="/" component={AirHockey} />
             <Route path="/asteroids" component={Asteroids} />
-            <Route path="/physics-network" component={AirHockey} />
+            <Route path="/physics-network" component={AirHockeyNetwork} />
             <Route path="/air-hockey/local" component={AirHockeyLocal} />
             <Route path="/air-hockey/network" component={AirHockeyNetwork} />
+            <Route component={AirHockeyLocal} />
         </Switch>
     </main>
 );
